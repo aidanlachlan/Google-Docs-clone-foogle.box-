7dbbd67d45afa64f955859ecd6c9019112d1fa1f
Aidan <aidanlmcmurray@gmail.com>
2025-01-23 10:10:27 -0800

    initialized

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+import ReactQuill from "react-quill";
+import { setDoc, doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase-config";
+import 'react-quill/dist/quill.snow.css';
+import "../App.css";
+import {throttle} from "lodash"
+
+export const TextEditor = () => {
+    const quillRef = useRef<any>(null);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
+    const isLocalChange = useRef(false)
+
+    const documentRef = doc(db, "documents", "sample-doc");
+
+    const saveContent = throttle(() => {
+        if (quillRef.current && isLocalChange.current) {
+            const content = quillRef.current.getEditor().getContents();
+            console.log(`Saving conent to db: `, content)
+
+            setDoc(documentRef, { content: content.ops }, { merge: true })
+            .then(() => console.log("content saved"))
+            .catch(console.error);
+            isLocalChange.current = false;
+        }
+    }, 1000);
+
+    useEffect(() => {
+        if (quillRef.current) {
+            // Load initital content from firestore db
+
+            getDoc(documentRef).then((docSnap) => {
+                if (docSnap.exists()) {
+                    const savedContent = docSnap.data().content;
+                    if (savedContent) {
+                        quillRef.current.getEditor().setContents(savedContent);
+                    }
+                }
+                else {
+                    console.log("No doc found, starting with an empty editor")
+                }
+
+            }).catch(console.error);
+
+            // Listen to Firestore for any updates and updae locally in real-time
+
+            const unsubscribe = onSnapshot(documentRef, (snapshot) => {
+                if (snapshot.exists()) {
+                    const newContent = snapshot.data().content
+
+                    if (!isEditing) {
+                        const editor = quillRef.current.getEditor()
+                        const currentCursorPosition = editor.getSelection()?.index || 0
+
+                        editor.setContents(newContent, "silent");
+                        editor.setSelection(currentCursorPosition);
+                    }
+                }
+            })
+
+            // Listen for local text changes and save it to firestore 
+            const editor = quillRef.current.getEditor();
+            editor.on("text-change", (delta: any, oldDelta: any, source: any) => {
+                if (source === "user") {
+                isLocalChange.current = true;
+                setIsEditing(true);
+                saveContent();
+
+                setTimeout(() => setIsEditing(false), 5000);
+                }
+            });
+
+            return () => {
+                unsubscribe()
+                editor.off("text-change");
+            }
+
+        }
+    }, [])
+
+    return <>
+        <div className="google-docs-editor">
+            <ReactQuill ref={quillRef}/>
+        </div>
+
+    </>;
+};
---
+++ src/firebase-config.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbPRHUSgZjBeiIML2p7j5gnmBkBVz43ts",
+  authDomain: "docs-clone-tut-73e3e.firebaseapp.com",
+  projectId: "docs-clone-tut-73e3e",
+  storageBucket: "docs-clone-tut-73e3e.firebasestorage.app",
+  messagingSenderId: "1064202113883",
+  appId: "1:1064202113883:web:7d942b9055b4bb3ff8407e",
+  measurementId: "G-RS9XPYTKM5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
